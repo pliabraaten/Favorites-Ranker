de5d84a101f21cf19b7ce2ddbd5c2c9fdea699d5
parker <pliabraaten@gmail.com>
2025-08-06 21:38:27 -0700

    Add Spring Boot project files

---
+++ src/main/java/com/PL/ranker_app/RankerAppApplication.java
@@ -0,0 +1,13 @@
+package com.PL.ranker_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RankerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RankerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PL/ranker_app/RankerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PL.ranker_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
