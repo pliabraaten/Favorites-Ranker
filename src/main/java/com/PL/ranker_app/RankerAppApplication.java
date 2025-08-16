429a7ddfa07ab546f0803677b62bc9987e9dc679
parker <pliabraaten@gmail.com>
2025-08-15 21:08:37 -0700

    Add Menu object to run the CLI

--- src/main/java/com/PL/ranker_app/RankerAppApplication.java
+++ src/main/java/com/PL/ranker_app/RankerAppApplication.java
@@ -14,55 +14,11 @@ public class RankerAppApplication {
 	}
 	{
 		// START CLI MENU
-		menu();
+		Menu menu = new Menu();
+		menu.startMenu();
 
 
 	}
 
-	// COMMAND LINE MENU
-	void menu() {
 
-		Scanner scanner = new Scanner(System.in);
-		String listString = null;
-		String action = null;
-
-		// Keeper user in menu until they exit by entering 0
-		do {
-			System.out.println("Welcome to the Ranker Project\n"
-								+ "Type 1 to enter list of items to be ranked\n"
-								+ "Type 0 to exit program"
-			);
-
-			// Retrieve action input from user
-			action = scanner.nextLine();
-
-			// If 1 save input as string
-			if (action.equals("1")) {
-
-				// Prompt user to enter comma separated list
-				System.out.println("Enter list (ex: item1, item2, ...");
-				listString = scanner.nextLine();
-
-//				System.out.println("inputted list: " + listString);
-
-				// Create ItemList based on user input
-				ItemList itemList = new ItemList(listString);
-
-				itemList.printList();
-
-				// Compare
-				itemList.compareItems();
-//				ArrayList<String> rankedList = BinaryInsertionSort.compareItems(String[] itemList.parsedList);
-
-				// Print output
-				itemList.printRankedList();
-
-			}
-		}
-		while (!action.equals("0"));
-
-		// If 0 action, exit program
-		System.exit(0);
-
-	}
 }
