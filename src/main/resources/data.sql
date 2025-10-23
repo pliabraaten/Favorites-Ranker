064b49f2cd98f5b19c71fa776cf77eca5c96a54b
parker <pliabraaten@gmail.com>
2025-10-23 07:32:26 -0700

    Adds H2 in-memory database configuration

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+
+
+
+# prepopulate the database
+INSERT INTO rankedList (id, name) VALUES (1, 'pizza');
+INSERT INTO rankedList (id, name) VALUES (2, 'corn');
+INSERT INTO rankedList (id, name) VALUES (3, 'tortillas');
